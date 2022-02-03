53353aa9777ad4e6093a967e98a8e1c24add6316
trecho26 <hectorlara260698@gmail.com>
2022-02-02 21:51:20 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  let initialValues = {
+    nombre: "",
+    edad: 0,
+    correo: "",
+  };
+  const [values, setValues] = useState(initialValues);
+  const [users, setUsers] = useState([]);
+
+  const handleChange = (event) => {
+    let nameProperty = event.target.name;
+    setValues({
+      ...values,
+      [nameProperty]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    // Prevenir envio al servidor
+    event.preventDefault();
+
+    //Logica
+    setUsers([...users, values]);
+
+    // Limpiar los inputs
+    setValues(initialValues);
+  };
+
+  useEffect(() => {
+    // Logica de exclusion
+    if (users.length === 0) return;
+
+    // Impresion en consola
+    console.log("Agregaste un usuario");
+
+    // Agregar dependencias necesarias para escuchar a un estado en particular
+  }, [users]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="nombre"
+          value={values.nombre}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="edad"
+          value={values.edad}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="correo"
+          value={values.correo}
+          onChange={handleChange}
+        />
+        <input type="submit" value="Enviar" />
+      </form>
+
+      <h1>Usuarios totales {users.length}</h1>
+
+      {users.map((user, index) => (
+        <div key={index}>
+          <p>{user.nombre}</p>
+          <p>{user.edad}</p>
+          <p>{user.correo}</p>
+        </div>
+      ))}
     </div>
   );
 }
